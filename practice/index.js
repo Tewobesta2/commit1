d4c903183d8e07ccc8b7a48ff4c09c9386e7d8f1
Tewobesta2 <atewobesta@gmail.com>
2026-04-27 00:57:29 +0300

    Add files via upload

--- practice/index.js
+++ practice/index.js
@@ -1,50 +1,95 @@
 const http = require('http');
-const movies = require('./movie'); 
+const movies = require('./movie');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
 
-    if (req.url === '/api/movie' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(movies));
-    }
-    else if (req.url.startsWith('/api/movie/') && req.method === 'GET') {
-        const movieId = req.url.split('/')[3];
-        const movie = movies.find(m => m.id === parseInt(movieId));
-
-        if (movie) {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(movie));
-        } else {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Movie Not Found');
-        }
-    }
-
-    else if (req.url === '/api/add-movie' && req.method === 'POST') {
+    if (req.url === '/api/update-movie' && req.method === 'PUT') {
         let body = '';
 
         req.on('data', chunk => {
             body += chunk.toString();
         });
 
-        req.on('end', () => {
-            const newMovie = JSON.parse(body);
+        req.on('end', async () => {
+            const updatedMovie = JSON.parse(body);
+            const result = await updateMovie(updatedMovie);
 
-            const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
-            newMovie.id = newId;
-
-            movies.push(newMovie);
-
-            res.writeHead(201, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(newMovie));
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(result));
         });
     }
 
+    else if (req.url.startsWith('/api/delete-movie/') && req.method === 'DELETE') {
+        (async () => {
+            const movieId = req.url.split('/')[3];
+            const result = await deleteMovie(parseInt(movieId));
+
+            if (result) {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(result));
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Movie Not Found');
+            }
+        })();
+    }
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
     }
-}); 
+});
+
+const updateMovie = async (movie) => {
+    const movieDataPath = path.join(__dirname, 'movie.js');
+
+    let data = await fs.promises.readFile(movieDataPath, 'utf-8');
+
+    data = data.replace('const movies = ', '');
+    data = data.replace('; module.exports = movies;', '');
+    data = data.replace('module.exports = movies;', '');
+
+    let moviesArray = eval(data);
+
+    const index = moviesArray.findIndex(m => m.id === movie.id);
+
+    if (index !== -1) {
+        moviesArray[index] = movie;
+    }
+
+    const fileData = `const movies = ${JSON.stringify(moviesArray, null, 4)};\n\nmodule.exports = movies;`;
+
+    await fs.promises.writeFile(movieDataPath, fileData);
+
+    return movie;
+}
+
+const deleteMovie = async (id) => {
+    const movieDataPath = path.join(__dirname, 'movie.js');
+
+    let data = await fs.promises.readFile(movieDataPath, 'utf-8');
+
+    data = data.replace('const movies = ', '');
+    data = data.replace('; module.exports = movies;', '');
+    data = data.replace('module.exports = movies;', '');
+
+    let moviesArray = eval(data);
+
+    const index = moviesArray.findIndex(m => m.id === id);
+
+    if (index !== -1) {
+        const deletedMovie = moviesArray.splice(index, 1)[0];
+
+        const fileData = `const movies = ${JSON.stringify(moviesArray, null, 4)};\n\nmodule.exports = movies;`;
+
+        await fs.promises.writeFile(movieDataPath, fileData);
+
+        return deletedMovie;
+    }
+
+    return null;
+}
 
 const PORT = 5000;
 
